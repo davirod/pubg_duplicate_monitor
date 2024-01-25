b819f07ec43963232df28ee23ef741d6abd58729
Davi Rodrigues <davi.rodrigues@servian.com>
2024-01-25 13:20:36 +1100

    Didn't pay attention that I had extra old script after line 40. Added Readme.MD file

--- start_pubg_and_monitor.ps1
+++ start_pubg_and_monitor.ps1
@@ -1,6 +1,5 @@
 $gameProcessName = "TslGame"
 $gameSteamId = "578080"
-$alreadyRunning = $false
 $extraProcessTerminated = $false
 
 # OK, we need to first check if the game is already running
@@ -39,39 +38,3 @@ while (-not $extraProcessTerminated) {
 }
 
 exit
-
-
-
-
-
-while ($true) {
-    # Start monitoring
-    Start-Sleep -Seconds 10
-    # Check if TslGame.exe is running
-    $gameProcess = Get-Process $gameProcessName -ErrorAction SilentlyContinue
-    if ($gameProcess -and -not $alreadyRunning) {
-        # Run your script if the game has started and the script is not already running
-        # Check if there are multiple TslGame processes running
-        if ($gameProcess.Count -gt 1) {
-            # Sort the processes by working set memory (memory usage) and select the one using the least memory
-            $leastMemoryProcess = $gameProcess | Sort-Object WorkingSet -Descending | Select-Object -Last 1
-
-            # Terminate the process using the least memory
-            $leastMemoryProcess | Stop-Process
-            Write-Host "Terminated TslGame.exe with ID $($leastMemoryProcess.Id) using the least memory."
-            $extraProcessTerminated = $true
-        } else {
-            Write-Host "Only one or no TslGame.exe process found."
-        }
-    } elseif (-not $gameProcess) {
-        # Reset the flag if the game is not running
-        $alreadyRunning = $false
-    }
-
-    if ($extraProcessTerminated) {
-        exit
-    }
-
-    # Wait for a bit before checking again
-    Start-Sleep -Seconds 10
-}
